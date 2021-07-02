eef29f0fc8b94960e2c32816412a253190251fa2
Elvis Perez <ebardalesdj@mpfn.gob.pe>
2021-07-02 00:05:19 -0500

    Factorizacion para inyeccion de filtro

--- src/main/java/pe/gob/mpfn/security/config/JwtAuthenticationFilter.java
+++ src/main/java/pe/gob/mpfn/security/config/JwtAuthenticationFilter.java
@@ -18,10 +18,12 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import pe.gob.mpfn.security.enumeration.RuvaEnum;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter
 {
 
